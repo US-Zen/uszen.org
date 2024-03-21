bb087c34754128c7df89c62dd906157f61bf2a4a
Amar Dinsa <dinsaa@gmail.com>
2024-03-21 13:48:19 -0400

    Initial commit with basic page layout

---
+++ tailwind.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  content: ['./src/**/*.html'],
+  plugins: [require("daisyui"), require("@tailwindcss/forms"), require("@tailwindcss/typography")]
+}
---
+++ webpack.config.js
@@ -0,0 +1,73 @@
+const path = require('path');
+const HtmlBundlerPlugin = require('html-bundler-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+
+const isProd = !process.argv.find((str) => str.includes('development'));
+
+module.exports = {
+  mode: isProd ? 'production' : 'development',
+  devtool: isProd ? 'source-map' : 'inline-source-map',
+  stats: 'minimal',
+
+  output: {
+    path: path.join(__dirname, 'dist/'),
+    clean: true,
+  },
+
+  plugins: [
+    new HtmlBundlerPlugin({
+      entry: {
+        index: 'src/views/index.html',
+        about: 'src/views/about.html',
+        support: 'src/views/support.html',
+        contact: 'src/views/contact.html'
+      },
+      js: {
+        // output filename of compiled JavaScript
+        filename: 'js/[name].[contenthash:8].js',
+      },
+      css: {
+        // output filename of extracted CSS
+        filename: 'css/[name].[contenthash:8].css',
+      },
+      preprocessor: 'nunjucks', // use the Nunjucks template engine
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: "src/documents", to: "documents" }
+      ],
+    }),
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.(css|scss)/,
+        use: ['css-loader', 'postcss-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(ico|png|jp?g|svg)$/,
+        type: 'asset/resource',
+        generator: {
+          filename: 'img/[name].[hash:8][ext]',
+        },
+      },
+    ],
+  },
+
+  // enable live reload
+  devServer: {
+    open: true,
+    compress: true,
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    static: path.resolve(__dirname, 'dist'),
+    watchFiles: {
+      paths: ['src/**/*.*'],
+      options: {
+        usePolling: true,
+      },
+    },
+  },
+};
